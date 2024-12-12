8b8865641c6ead3ffda9c91a0fac222ea116c0a4
Taufiq <taufiq9.2004@gmail.com>
2024-12-12 17:46:12 +0530

    postgres and prisma setup

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,11 +1,19 @@
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
 import express from 'express';
+import { PrismaClient } from '@prisma/client'
 
 export default async function graphqlServer() {
 
 const app = express();
 
+const prisma = new PrismaClient()
+const newUser = await prisma.user.create({
+  data: {
+    name: 'Alice',
+    email: 'alice@prisma.io',
+  },
+})
 
 const server = new ApolloServer({
   typeDefs:`
